0422f68e321236d3495617a122a297014e2349ab
galhrn <gala@codeoasis.com>
2022-05-09 11:25:23 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Channel as StreamChannel, StreamChat } from "stream-chat";
+import "@stream-io/stream-chat-css/dist/css/index.css";
 
-function App() {
+import {
+  Chat,
+  Channel,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  LoadingIndicator,
+} from "stream-chat-react";
+import "./App.css";
+
+const apiKey = process.env.REACT_APP_STREAM_API_KEY ?? "";
+
+const user = {
+  id: "1",
+  name: "Gal aharon",
+  image: "https://getstream.imgix.net/images/random_svg/FS.png",
+};
+
+const App = () => {
+  const [client, setClient] = useState<StreamChat | null>(null);
+  const [channel, setChannel] = useState<StreamChannel | null>(null);
+
+  const { name, image } = user;
+
+  useEffect(() => {
+    const { id: userId } = user;
+
+    (async () => {
+      const chatClient = StreamChat.getInstance(apiKey);
+      await chatClient.connectUser(user, chatClient.devToken(userId));
+
+      const channel = chatClient.channel("messaging", "channel-id");
+      await channel.watch();
+
+      setChannel(channel);
+      setClient(chatClient);
+    })();
+
+    // if (client) return () => client.disconnectUser();
+  }, [client]);
+
+  if (!channel || !client) return <LoadingIndicator />;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chat client={client}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   );
-}
+};
 
 export default App;
